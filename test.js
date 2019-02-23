22681657eff026f117225f873a47e786a1123e6c
perkinsjr <james.r.perkins@tuta.io>
2019-02-23 14:53:37 -0500

    adding test

--- test.js
+++ test.js
@@ -5,12 +5,18 @@ test('DateSlashes', t => {
     const mapTest = regexify('01/01/2014','DateSlashes');
     t.is(true,mapTest);
 } )
+
+test('Time24hr', t => {
+    const mapTest = regexify('19:00','Time24hr');
+    t.is(true,mapTest);
+} )
+
 test('Visa', t => {
     const mapTest = regexify('4111756955551234','VISA');
     t.is(true,mapTest);
 } )
 
 test('nopattern', t => {
-    const mapTest = regexify('049-13-7556','NOTHING');
+    const mapTest = regexify('011-55-9087','NOTHING');
     t.is(mapTest,'no pattern ' + 'NOTHING' + ' available');
 } )
